e59a6d36600d33d57925673b4da127bf31a8b2f9
Angel <aamayamartin@safareyes.es>
2023-02-08 17:06:17 +0100

    actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UsersService} from "./services/users.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'e-pets-angular';
+
+  constructor(private usersService: UsersService) {
+    console.log('El componente se ha creado');
+  }
+
+  ngOnInit(): void {
+    console.log('El componente se ha inicializado');
+    let response;
+    this.usersService.getUsers()
+      .subscribe(console.log(response));
+  }
+
 }
