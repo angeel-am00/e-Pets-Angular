97a248b81eb06fc19496dfc0c4f4d4b1314b5963
david <ddominguezcampuzano@safareyes.es>
2023-02-24 20:20:50 +0100

    eee

---
+++ src/app/serprotectora/serprotectora.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import {faHouse} from "@fortawesome/free-solid-svg-icons";
+import {faComments} from "@fortawesome/free-solid-svg-icons";
+import {faUsers} from "@fortawesome/free-solid-svg-icons";
+import {faShieldDog} from "@fortawesome/free-solid-svg-icons";
+import {faDoorOpen} from "@fortawesome/free-solid-svg-icons";
+import {faUserPen} from "@fortawesome/free-solid-svg-icons";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import {faCheck} from "@fortawesome/free-solid-svg-icons";
+import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import {Router} from "@angular/router";
+import {faPencil} from "@fortawesome/free-solid-svg-icons";
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+@Component({
+  selector: 'app-serprotectora',
+  templateUrl: './serprotectora.component.html',
+  styleUrls: ['./serprotectora.component.css']
+})
+export class SerprotectoraComponent {
+
+  constructor(private router:Router) {
+  }
+  user(){
+    this.router.navigate(["users"])
+  }
+  protectora(){
+    this.router.navigate(["protectora"])
+  }
+  home(){
+    this.router.navigate(["home"])
+  }
+
+  miperfil(){
+    this.router.navigate(["miperfil"])
+  }
+
+  faHouse = faHouse;
+  faComments = faComments;
+
+  faUsers = faUsers;
+
+  faShieldDog = faShieldDog;
+
+  faDoorOpen= faDoorOpen;
+
+  faUserPen = faUserPen;
+  faMagnifyingGlass= faMagnifyingGlass;
+
+  faCheck=faCheck;
+
+  faTimes = faTimes;
+
+  faPenSquare=faPencil;
+  faArrowLeft=faArrowLeft
+
+}
+
